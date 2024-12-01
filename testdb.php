3c30f781235ba3623c215c77fd56e26282950a96
omarprojects99 <omarbenhajsaid1@gmail.com>
2024-12-01 01:32:00 +0100

    Initial commit

---
+++ testdb.php
@@ -0,0 +1,18 @@
+<?php
+$servername="localhost";
+$DBuser="root";
+$DBpassword="";
+$DBname ="ecoshoe";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$DBname", $DBuser,$DBpassword);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+
+
+?>
